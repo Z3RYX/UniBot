170b8f126630b70c516882c43314629b51440307
Z3RYX <39765816+Z3RYX@users.noreply.github.com>
2018-06-27 11:25:05 +0200

    Added 2 commands
    universe missing a variable for now

--- Modules/C.cs
+++ Modules/C.cs
@@ -1,10 +1,24 @@
-﻿using System;
+﻿using Discord.Commands;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace UniBot.Modules
 {
-    class C
+    [Group("c")]
+    public class C : ModuleBase<SocketCommandContext>
     {
+        [Command("ping")]
+        public async Task CPing()
+        {
+            await ReplyAsync("Who pinged me?");
+        }
+		
+		[Command("universe")]
+		public async Task CUniverse()
+		{
+			await ReplyAsync($"There are {} solar systems in this universe");
+		}
     }
 }
